d28949af5a689124e114a03a551f1a450ba37523
lkeey <leon062007@mail.ru>
2023-07-15 20:49:23 +0300

    create api

---
+++ app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "9f00bd0ecbdc45e8b73106da41d8d85d"
+        const val BASE_URL = "https://newsapi.org"
+
+    }
+}
