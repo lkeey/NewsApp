f958961996376d7e9c73822f77e70d83cea44136
lkeey <leon062007@mail.ru>
2023-07-15 22:20:35 +0300

    clear cache

--- app/src/main/java/com/androiddevs/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/newsapp/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package com.androiddevs.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.newsapp.R
-import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.hostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // set up navigation
-        bottomNavigationView.setupWithNavController(hostFragment.findNavController())
+
+        var nav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        nav.setupWithNavController(navController)
     }
 }
